568452955e8a7efd43843df457d4e8bc939396ff
Sivanildo F Moreira <sir.sivanildofm@gmail.com>
2020-11-16 13:40:44 -0300

    Mensagem a ser apagada

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -5,5 +5,6 @@ public class Program {
 	public static void main(String[] args) {
 		
 		System.out.println("Tamo tentando fazer funcionar");
+		System.out.println("Vamo fazer pra apagar depois");
 	}
 }
